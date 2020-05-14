b985ec671103cad26f8c170abcd085784a308156
12FrosT <ashishchauhan1206@gmail.com>
2020-05-15 01:20:47 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { UserLoginComponent } from './user-login/user-login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/user/login', pathMatch: 'full' },
+  { path: 'user/login', component: UserLoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
